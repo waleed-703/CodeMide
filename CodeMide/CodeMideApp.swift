ef0eb3c4ebfe38a62297820a0cee245858fe1d40
waleed-703 <waleedahmedkhan703@gmail.com>
2026-03-03 22:41:41 +0500

    Initial Commit

---
+++ CodeMide/CodeMideApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeMideApp.swift
+//  CodeMide
+//
+//  Created by Mudassir Ahmed Khan on 03/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CodeMideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeMide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeMide
+//
+//  Created by Mudassir Ahmed Khan on 03/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
